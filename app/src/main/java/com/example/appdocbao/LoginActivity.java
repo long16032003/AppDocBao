11548704015faebd0b1467fe95231d4c94be7828
gialong <longnguyengia890@gmail.com>
2024-02-25 17:28:46 +0700

    update sign up activity

--- app/src/main/java/com/example/appdocbao/LoginActivity.java
+++ app/src/main/java/com/example/appdocbao/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.example.appdocbao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText txtEmail;
+    AppCompatButton btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtEmail = (EditText) findViewById(R.id.userEdt);
+        btnLogin = (AppCompatButton) findViewById(R.id.loginBtn);
     }
 }
