40b2381ff0e2e3928bf6edfc3b8a12cc141592a5
DuongHoangR1pper <duonghoangripper25@gmail.com>
2024-02-26 23:18:21 +0700

    update menu bottom nav

--- app/src/main/java/com/example/appdocbao/LoginActivity.java
+++ app/src/main/java/com/example/appdocbao/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.appdocbao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
