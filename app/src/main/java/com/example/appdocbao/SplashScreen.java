233f845e4fa37c88057a4d43873141fe9034f442
Long <longnguyengia890@gmail.com>
2024-02-28 15:37:49 +0700

    addfunction: Splash Screen

---
+++ app/src/main/java/com/example/appdocbao/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.appdocbao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
