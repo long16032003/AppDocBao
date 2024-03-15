b1697c247da8184dc90249ebd0d5b258769a4338
tokinooru4424 <minhduc2465@gmail.com>
2024-03-15 23:50:39 +0700

    upgrade DetailArticleActivity

--- app/src/main/java/com/example/appdocbao/Activity/MainActivity.java
+++ app/src/main/java/com/example/appdocbao/Activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.appdocbao.Activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
