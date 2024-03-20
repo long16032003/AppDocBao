e0101e0db5a11e2737f7a920ecbba8496b3fe2a3
Long <longnguyengia890@gmail.com>
2024-03-20 19:49:20 +0700

    update user on realtime DB and forget password getCurrentUser

--- app/src/main/java/com/example/appdocbao/Activity/MainActivity.java
+++ app/src/main/java/com/example/appdocbao/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appdocbao.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -11,22 +12,28 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.appdocbao.BroadcastReceiver.Internet;
 import com.example.appdocbao.Fragment.GifFragment;
 import com.example.appdocbao.Fragment.NewsFragment;
 import com.example.appdocbao.Fragment.ProfileFragment;
 import com.example.appdocbao.Fragment.TrendFragment;
 import com.example.appdocbao.R;
 import com.example.appdocbao.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     DrawerLayout drawerLayout;
+    private Internet internetBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        internetBroadcastReceiver = new Internet();
 
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -55,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-
         replaceFragement(new NewsFragment());
         binding.bottomNavigationView.setBackground(null);
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -67,7 +73,13 @@ public class MainActivity extends AppCompatActivity {
             } else if (itemId == R.id.trend) {
                 replaceFragement(new TrendFragment());
             } else if (itemId == R.id.profile) {
-                replaceFragement(new ProfileFragment());
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                    startActivity(intent);
+                }else{
+                    replaceFragement(new ProfileFragment());
+                }
             }
             return true;
         });
