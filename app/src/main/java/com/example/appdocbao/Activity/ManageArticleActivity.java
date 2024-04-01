73759cdcf7c49c8f5bc88c637167a848f4f2deda
Long <longnguyengia890@gmail.com>
2024-04-01 17:51:49 +0700

    chinh lai giao dien chi tiet bai bao

---
+++ app/src/main/java/com/example/appdocbao/Activity/ManageArticleActivity.java
@@ -0,0 +1,16 @@
+package com.example.appdocbao.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appdocbao.R;
+
+public class ManageArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_article);
+    }
+}
